b389899156035e1a8807203337e5f055451bd33e
simplety <simplety1031@gmail.com>
2017-08-28 18:20:29 +0800

    no change

--- Object-oriented/Tab/tab.js
+++ Object-oriented/Tab/tab.js
@@ -2,15 +2,28 @@
  * Created by Kaisla on 10/08/2017.
  */
 
-
 Tab = (function(){
-    function _Tab(container){
+    function _Tab(container,tabConfig){
+        this.baseConfig = {
+            tabList : {
+                title : "tab1",
+                content : "content 1"
+            },
+            activeIndex : 0
+        };
         this.container = container;
+        this.tabConfig = Object.assign({},baseConfig,tabConfig)
+
         this.init();
         this.bind();
     }
 
     _Tab.prototype = {
+        render: function(this.container){
+            this.tabConfig = {
+                this.tabList
+            }
+        },
         init: function(){
             console.log("container",this.container);
             this.tabTitleWrap = this.container.querySelector(".tab-nav");
